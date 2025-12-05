dc31e3ce6e0bb92fee0eff9b3c64798f5bf358cd
tepid <tepschmep@gmail.com>
2025-12-04 22:24:57 -0300

    2025: day 2, slow

--- 2025/lua/day_02.lua
+++ 2025/lua/day_02.lua
@@ -9,14 +9,12 @@ local function part_one()
 
     for line in input.lines(DAY) do
         for _, range in pairs(line:split(",")) do
-            local lower, upper = table.unpack(range:split("-"))
+            local lower, upper = range:match("(%d+)%-(%d+)") -- hell on earth
 
             for i = tonumber(lower), tonumber(upper) do
                 if string.len(i) % 2 == 0 then
                     local x, y = string.slice(i, string.len(i) // 2)
-                    if x == y then
-                        id_sum = id_sum + i
-                    end
+                    if x == y then id_sum = id_sum + i end
                 end
             end
         end
@@ -28,7 +26,33 @@ end
 local function part_two()
     local id_sum = 0
 
-    -- Something will happen
+    for line in input.lines(DAY) do
+        for _, range in pairs(line:split(",")) do
+            local lower, upper = range:match("(%d+)%-(%d+)") -- hell on earth
+
+            for i = tonumber(lower), tonumber(upper) do
+                for j = 1, string.len(i) // 2 do -- i has no divisors over i//2 besides itself
+                    if string.len(i) % j == 0 then
+                        local is_invalid = true
+                        for cursor = 1, string.len(i) - j, j do
+                            local x = string.sub(i, cursor, cursor - 1 + j)
+                            local y = string.sub(i, cursor + j, cursor - 1 + 2 * j)
+
+                            if x ~= y then
+                                is_invalid = false
+                                break
+                            end
+                        end
+
+                        if is_invalid then
+                            id_sum = id_sum + i
+                            break
+                        end
+                    end
+                end
+            end
+        end
+    end
 
     return id_sum
 end

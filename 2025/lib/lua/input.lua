efda779b71a402eb5b76cc9118edf8e369119425
tepid <tepschmep@gmail.com>
2025-12-23 16:35:56 -0300

    2025: simplified input functions
    -> removed unnecessary `input` table
    -> the `input (day)` function just returns an iterator over all input lines
    -> now properly closing the file handle after i'm done

--- 2025/lib/lua/input.lua
+++ 2025/lib/lua/input.lua
@@ -1,63 +1,44 @@
-input = {}
+-- Directories the get_file function attempts to check.
+-- If the input file is not here, something is wrong.
+local valid_directories <const> = { "2025/input", "input", "../input", "../../input" }
 
--- Gets the input file associated with `day`.
---
--- Attempts to read from `[day].in` files in the `2025/input/` directory,
--- and expects days in the file names to be written with two digits (ie. `01.in`).
-local function get_file(day)
-    local guesses = { "2025/input", "input", "../input", "../../input" }
+-- Gets the input file associated with the specified day.
+-- Attempts to read from '{day}.in' files, where the day is written with two digits (ie. '01.in').
+local function get_file (day)
+	local file, err
 
-    day = tonumber(day)
-    for _, line in ipairs(guesses) do
-        local filename = string.format(
-            "%s/%d%d.in",
-            line,
-            day // 10, -- first digit
-            day % 10   -- second digit
-        )
+	for _, guess in pairs(valid_directories) do
+		local filename = string.format(
+			"%s/%d%d.in",
+			guess,
+			day // 10, -- first digit
+			day % 10   -- second digit
+		)
 
-        local file = io.open(filename, "r") -- tests if the file exists
-        if file then
-            return file
-        end
-    end
+		file, err = io.open(filename, "r")
+		if file then return file end
+	end
 
-    error("ERROR - File not found")
-    return nil
+	error("Error: " .. err)
 end
 
--- Returns an array with each line of the input associated with `day`.
---
--- Throws an error if the input file cannot be found.
-function input.get(day)
-    local file = get_file(day)
-    local input, i = {}, 1
-    if file then
-        repeat
-            local line = file:read("l")
-            input[i] = line
-            i = i + 1
-        until not line
+-- Returns an iterator over the lines of an input associated with the specified day.
+function input (day)
+	local file <close> = get_file(day)
+	
+	local i, lines = 1, {}
+	repeat
+		line = file:read()
+		lines[i] = line
+		i = i + 1
+	until line == nil
 
-        return input
-    end
+	file:close()
 
-    return nil
-end
-
--- Returns an iterator function for the input associated with `day`.
--- Allows for `for line in input.lines(day) do ... end`.
---
--- Throws an error if the input file cannot be found.
-function input.lines(day)
-    local input = input.get(day)
-
-    local i = 0
-    local n = #input -- length of input
-    if input then
-        return function()
-            i = i + 1
-            if i <= n then return input[i] end
-        end
-    end
+	-- iterator function 
+	counter = 0
+	return function ()
+		counter = counter + 1
+		if counter <= #lines then return lines[counter] end
+	end
 end

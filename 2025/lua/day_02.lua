5ceef2c394940370a5ed6920f83ebf36954ab575
tepid <tepschmep@gmail.com>
2025-12-25 22:05:01 -0300

    2025: day 2, with math instead

--- 2025/lua/day_02.lua
+++ 2025/lua/day_02.lua
@@ -1,49 +1,82 @@
 -- Please execute this script in the directory `2025/lua`. This will break otherwise
 dofile("../lib/lua/input.lua")
-dofile("../lib/lua/string_extensions.lua")
+
+local function get_digit_count (num)
+    return math.floor(math.log(num, 10) + 1)
+end
+
+local function get_digit (num, index, digit_count --[[optional, if you know it ahead of time]])
+    digit_count = digit_count or get_digit_count(num)
+
+    -- given a base-10 number with D digits, you can find its Kth digit by computing (number // 10^(D - K)) % 10
+    return (num // 10^(digit_count - index)) % 10
+end
 
 local function part_one()
     local id_sum = 0
-
+   
+    local pattern = "(%d+)%-(%d+)" -- matches two digits separated by a '-' and captures both digits
     for line in input(2) do
-        for _, range in pairs(line:split(",")) do
-            local lower, upper = range:match("(%d+)%-(%d+)") -- hell on earth
+        for lower, upper in string.gmatch(line, pattern) do
+            for num = lower, upper do
+                local digit_count = get_digit_count(num) 
 
-            for i = tonumber(lower), tonumber(upper) do
-                if string.len(i) % 2 == 0 then
-                    local x, y = string.slice(i, string.len(i) // 2)
-                    if x == y then id_sum = id_sum + i end
+                if digit_count % 2 == 0 then
+                    local half = digit_count // 2
+                    local is_invalid = true
+
+                    for i = 1, half do 
+                        local a, b = get_digit(num, i, digit_count), get_digit(num, i + half, digit_count)
+
+                        if a ~= b then
+                            is_invalid = false
+                            break
+                        end
+                    end
+
+                    if is_invalid then
+                        id_sum = id_sum + num
+                    end
                 end
             end
         end
     end
 
-    return id_sum
+    return math.tointeger(id_sum) -- in case it's still a float
 end
 
 local function part_two()
     local id_sum = 0
 
+    local pattern = "(%d+)%-(%d+)" -- matches two digits separated by a '-' and captures both digits
     for line in input(2) do
-        for _, range in pairs(line:split(",")) do
-            local lower, upper = range:match("(%d+)%-(%d+)") -- hell on earth
+        for lower, upper in string.gmatch(line, pattern) do
+            for num = lower, upper do
+                local digit_count = get_digit_count(num) 
 
-            for i = tonumber(lower), tonumber(upper) do
-                for j = 1, string.len(i) // 2 do -- i has no divisors over i//2 besides itself
-                    if string.len(i) % j == 0 then
+                for mod = 1, digit_count // 2 do -- digit_count has no divisors over half besides itself
+                    if digit_count % mod == 0 then
+                        local segment = mod == 1 and 1 or digit_count // mod -- how many equally-sized segments we should divide the number in
                         local is_invalid = true
-                        for cursor = 1, string.len(i) - j, j do
-                            local x = string.sub(i, cursor, cursor - 1 + j)
-                            local y = string.sub(i, cursor + j, cursor - 1 + 2 * j)
 
-                            if x ~= y then
-                                is_invalid = false
-                                break
-                            end
+                        for n = 1, segment do -- check to see if every digit that leaves the same remainder when divided by n is equal. if this holds true, the number is invalid
+                            local j = n
+                            repeat 
+                                local a, b = get_digit(num, j, digit_count), get_digit(num, j + segment, digit_count)
+
+                                if a ~= b then
+                                    is_invalid = false
+                                    break
+                                end
+
+                                j = j + segment
+                            until j + segment > digit_count
+
+                            if not is_invalid then break end
                         end
 
                         if is_invalid then
-                            id_sum = id_sum + i
+                            id_sum = id_sum + num
                             break
                         end
                     end
@@ -52,7 +85,7 @@ local function part_two()
         end
     end
 
-    return id_sum
+    return math.tointeger(id_sum)
 end
 
 local function main()

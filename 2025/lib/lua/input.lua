1123bb6adfc34d25c0057d0a3acd55eb0c57d839
tepid <tepschmep@gmail.com>
2026-01-04 23:17:15 -0300

    2025: yet simpler input function

--- 2025/lib/lua/input.lua
+++ 2025/lib/lua/input.lua
@@ -5,40 +5,31 @@ local valid_directories <const> = { "2025/input", "input", "../input", "../../in
 -- Gets the input file associated with the specified day.
 -- Attempts to read from '{day}.in' files, where the day is written with two digits (ie. '01.in').
 local function get_file (day)
-	local file, err
+    local file, err
 
-	for _, guess in pairs(valid_directories) do
-		local filename = string.format(
-			"%s/%d%d.in",
-			guess,
-			day // 10, -- first digit
-			day % 10   -- second digit
-		)
+    for _, guess in pairs(valid_directories) do
+	local filename = string.format(
+            "%s/%d%d.in",
+	    guess,
+	    day // 10, -- first digit
+	    day % 10   -- second digit
+	)
 
-		file, err = io.open(filename, "r")
-		if file then return file end
-	end
+	file, err = io.open(filename, "r")
+	if file then return file end
+    end
 
-	error("Error: " .. err)
+    error("Error: " .. err)
 end
 
 -- Returns an iterator over the lines of an input associated with the specified day.
 function input (day)
-	local file <close> = get_file(day)
+    local file = get_file(day)
 	
-	local i, lines = 1, {}
-	repeat
-		line = file:read()
-		lines[i] = line
-		i = i + 1
-	until line == nil
+    return function ()
+        local line = file:read()
 
-	file:close()
-
-	-- iterator function 
-	counter = 0
-	return function ()
-		counter = counter + 1
-		if counter <= #lines then return lines[counter] end
-	end
+        if line then return line end
+        file:close()
+    end
 end
